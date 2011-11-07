1acf74bbd0f90080d46566d26ce8df91299c4fa5
Alexei Romanoff <drednout.by@gmail.com>
2011-11-07 14:03:28 +0000

    1) New help page 2) refactor: @base_title introduced in pages_controller_spec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe PagesController do
   integrate_views
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
   #Delete these examples and add some real ones
   it "should use PagesController" do
@@ -17,7 +20,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      response.should have_tag("title", "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_tag("title", @base_title + " | Home")
     end
   end
 
@@ -29,7 +32,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'contact'
-      response.should have_tag("title", "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_tag("title", @base_title + " | Contact")
     end
   end
 
@@ -41,7 +44,19 @@ describe PagesController do
 
     it "should have the right title" do
       get 'about'
-      response.should have_tag("title", "Ruby on Rails Tutorial Sample App | About")
+      response.should have_tag("title", @base_title + " | About")
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_tag("title", @base_title + " | Help")
     end
   end
 
